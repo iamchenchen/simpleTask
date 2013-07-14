8ee0bc6081c33f0bb245ad59e13a4afa256d7328
ibyon <imjubyon@gmail.com>
2013-07-13 20:17:10 -0700

    implemented insert

--- View/iDCCreateTaskViewController.h
+++ View/iDCCreateTaskViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "StackMob.h"
 
 @interface iDCCreateTaskViewController : UIViewController
 
--- View/iDCCreateTaskViewController.m
+++ View/iDCCreateTaskViewController.m
@@ -8,6 +8,8 @@
 
 #import "iDCCreateTaskViewController.h"
 #import "Task.h"
+#import "iDCAppDelegate.h"
+
 
 @interface iDCCreateTaskViewController ()
 
@@ -15,10 +17,16 @@
 
 @implementation iDCCreateTaskViewController
 
+- (iDCAppDelegate *)appDelegate {
+    return (iDCAppDelegate *)[[UIApplication sharedApplication] delegate];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.managedObjectContext = [[self.appDelegate coreDataStore] contextForCurrentThread];
+    
 }
 
 - (IBAction)createTask:(UIBarButtonItem *)sender {
@@ -30,7 +38,23 @@
     NSString *taskDetail = self.details.text;
     
     NSLog(@"taskTitle:%@, taskDetail:%@",taskTitle, taskDetail);
+    //  NSDate *now = [NSDate date];
+    //    [newManagedObject setValue:now forKey:@"createddate"];
+    //    [newManagedObject setValue:now forKey:@"lastmoddate"];
+    [newManagedObject setValue:taskTitle forKey:@"title"];
+    [newManagedObject setValue:taskDetail forKey:@"detail"];
+    [newManagedObject setValue:[newManagedObject assignObjectId] forKey:[newManagedObject
+                                                                         primaryKeyField]];
     
+    [self.managedObjectContext saveOnSuccess:^{
+        //      alter create successful
+        [self.navigationController popViewControllerAnimated:YES];
+    } onFailure:^(NSError *error) {
+        //Something bad has ocurred
+        NSString *errorString = [error localizedDescription];
+        UIAlertView *errorAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [errorAlertView show];
+    }];
     
 }
 
